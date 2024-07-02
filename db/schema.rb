89fba63ee242269c80a1e65d7307899a17536f0d
matsuda0513 <gdluk4everythinguact@gmail.com>
2024-07-02 22:20:31 +0900

    モデル、テーブル、ルーティング、コントローラーの設定、ビューファイルの格納

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_01_061501) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_02_123647) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", null: false
     t.string "encrypted_password", null: false
