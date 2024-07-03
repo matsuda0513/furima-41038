7aa70646b2ee39a6e8a404518bf669f9bf352c68
matsuda0513 <gdluk4everythinguact@gmail.com>
2024-07-03 11:24:31 +0900

    Active Storageコントローラー・モデルへの記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,10 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :image, :prefecture_id, :category_id, :condition_id, :postage_payer_id, :ship_date_estimate_id).merge(user_id: current_user.id)
+  end
 end
