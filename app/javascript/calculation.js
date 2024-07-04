f505e3d19bc469aad54a6f3b13a42b03449bcbd2
matsuda0513 <gdluk4everythinguact@gmail.com>
2024-07-04 22:18:10 +0900

    手数料の自動計算がリロードしないと反応しない件を修正

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -1,25 +1,18 @@
-  document.addEventListener('DOMContentLoaded', () => {
-    const priceInput = document.getElementById("item-price");
+const price = () => {
 
-    priceInput.addEventListener('input', (e) => {
-        const inputValue = parseFloat(priceInput.value);
-        const addTaxDom = document.getElementById('add-tax-price');
-        const profitDom = document.getElementById('profit');
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
 
-      if (isNaN(inputValue) || inputValue < 300 || inputValue > 9999999) {
-            addTaxDom.innerHTML = '';
-            profitDom.innerHTML = '';
-            console.log("価格は¥300から¥9,999,999の間で入力してください。");
-            return;
-        }
-        const tax = Math.floor(inputValue * 0.1);
-        const profit = inputValue - tax;
+    const addTaxDom = Math.floor(inputValue * 0.1);
+    const addTaxDomElement = document.getElementById("add-tax-price");
+    addTaxDomElement.textContent = addTaxDom;
 
-        addTaxDom.innerHTML = tax;
-        profitDom.innerHTML = profit;
-        
-        e.preventDefault();
+    const salesProfit = Math.floor(inputValue * 0.9);
+    const salesProfitElement = document.getElementById("profit");
+    salesProfitElement.textContent = salesProfit;
+  });
+};
 
-        console.log(`入力金額: ${inputValue}, 販売手数料: ${tax}, 販売利益: ${profit}`);
-    });
-});
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
